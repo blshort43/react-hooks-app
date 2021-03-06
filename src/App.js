c10482836b36a0d6e3e07e698294fd912c944597
blshort43 <blshort43@gmail.com>
2021-03-06 11:51:40 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BookContextProvider from "./contexts/BookContext";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
+    </BookContextProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookList from "./BookList";
+import BookForm from "./BookForm";
+
+const Dashboard = () => {
+  return (
+    <>
+      <BookList />
+      <BookForm />
+    </>
+  );
+};
+
+export default Dashboard;
